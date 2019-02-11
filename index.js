33efd6303751c9c4b834845b35d59b87377afe51
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-02-11 08:36:58 +0100

    sub function created

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const total = require('./src/scripts/js/addfunc');
+const diff = require('./src/scripts/js/subfunc');
 let numArray = [1, 2, 3, 4, 5];
 let [one, two, three, four, five] = [1, 2, 3, 4, 5];
 
@@ -8,3 +9,5 @@ console.log('the total of 1 + 2 + 3 + 4 + 5 = ', total.addn(...[one, two, three,
 console.log('the total of 1 + 2 + 3 + 4 + 5 = ', total.addn(one, two, three, four, five));
 
 console.log(numArray);
+
+console.log('the difference of 4 - 2: ', diff.sub(four, two));
---
+++ src/scripts/js/subfunc.js
@@ -0,0 +1,8 @@
+// This Function take two arguments and returns their difference as a single value
+
+function sub(a, b) {
+    return (a - b);
+}
+
+
+module.exports = { sub };
