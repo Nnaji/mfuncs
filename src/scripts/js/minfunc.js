44bed6fa203de711895142a1889c1e3e23e27a2c
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-02-11 14:16:14 +0100

    minimum value function completed

---
+++ src/scripts/js/minfunc.js
@@ -0,0 +1,15 @@
+// Min function takes a variable length of Arguments and return their min value
+
+function min(...args) {
+    nargs = args;
+    let mvSoFar = nargs[0];
+    // please not that I could have used a normal for-loop to achieve the same result.
+    for (i of nargs) {
+        if (i < mvSoFar) {
+            mvSoFar = i;
+        }
+    }
+    return (mvSoFar);
+}
+
+module.exports = { min };
