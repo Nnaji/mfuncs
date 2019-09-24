9aebc89be82ac70e7f89486c3abb2a3e4b8ea231
Kingsley Nnaji <kingsley.nnaji@gmail.com>
2019-09-24 14:06:39 +0200

    completed

---
+++ app.js
@@ -0,0 +1,9 @@
+/**************************************************************
+	@AUTHOR: Kingsley Nnaji <kingsley.nnaji@gmail.com>
+	@LICENSE: MIT
+	Creation Date : 24.09.2019
+***************************************************************/
+const math2 = require('./src');
+
+console.log(math2.add(2, 2));
+console.log(math2.sub(2, 2));
